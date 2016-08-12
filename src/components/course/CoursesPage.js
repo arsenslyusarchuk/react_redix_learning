82477bca6f79ca1b558f94dff465c1639ffb649f
Arsen Slyusarchuk <arsen.slyusarchuk@perfectial.com>
2016-08-12 15:14:11 +0300

    Redux flow (WITHOUT ASYNC)

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,7 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -22,13 +25,19 @@ class CoursesPage extends React.Component {
   }
 
   onFormSubmit() {
-    alert(`Submitting course ${this.state.course}`);
+    this.props.actions.createCourse(this.state.course);
+    // this.props.dispatch(courseActions.createCourse(this.state.course));
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
   }
 
   render() {
     return(
       <div>
         <h1>Course Page goes here...</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2> Add Course </h2>
         <input 
           type="text"
@@ -46,4 +55,38 @@ class CoursesPage extends React.Component {
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes =  {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired     
+};
+
+// -------------- Redux Section --------------
+
+// to access store data inside current component via props
+// like this.props.courses
+// we define that function
+// state.courses name comes from courseReducer.js
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+
+// to bind actions to our component 
+// we use import {bindActionCreators} from 'redux';
+// and it's syntax
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+
+// We haven't specified second param for connect function "mapDispatchToProps"
+// this is optional param that defines what actions we can dispatch from component
+// By ommiting it, redux inserts that automatically for us
+// -------------------------------------------------------------------------------
+// export default connect(mapStateToProps)(CoursesPage);
