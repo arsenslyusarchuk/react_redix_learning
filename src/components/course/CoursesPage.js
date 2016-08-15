7b60700204b1cdf5b4eada9ce12c3be39c81c97f
Arsen Slyusarchuk <arsen.slyusarchuk@perfectial.com>
2016-08-15 18:14:50 +0300

    Added async load of courses; Started implementing course form

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,31 +2,11 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: '' }
-    };
-
-    // instead of calling ".bind(this)" in the render
-    // which is less performance
-    // we do it here
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-  }
-
-  onTitleChange(event) {
-    let course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onFormSubmit() {
-    this.props.actions.createCourse(this.state.course);
-    // this.props.dispatch(courseActions.createCourse(this.state.course));
   }
 
   courseRow(course, index) {
@@ -34,22 +14,13 @@ class CoursesPage extends React.Component {
   }
 
   render() {
-    return(
+    // using ES6 object destruction
+    const {courses} = this.props;
+  
+    return (
       <div>
-        <h1>Course Page goes here...</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2> Add Course </h2>
-        <input 
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}
-        />
-
-        <input 
-          type="submit"
-          onClick={this.onFormSubmit}
-          value="save"
-        />
+        <h1>Courses</h1>
+        <CourseList courses={courses}/>
       </div>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,17 @@
 import {combineReducers} from 'redux';
 // because it's exported as default we can give whatever name we want
 import courses from './courseReducer';
+import authors from './authorReducer';
 
-// we coule use courses: courses
+// we could use courses: courses
 // but because it's ES6 we can use this 
 // shorthand property name
+// !!!!!!!!!!!!!!! IMPORTANT !!!!!!!!!!!!!!!
+// Also this name is the name which
+// u use in Container component
 const rootReducer = combineReducers({
-  courses
+  courses,
+  authors
 });
 
 export default rootReducer;
