3c3952a18f09f5d6f046d5067f32d9b7e01fb26b
Arsen Slyusarchuk <arsen.slyusarchuk@perfectial.com>
2016-08-16 10:56:45 +0300

    Created update/create course form

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -17,6 +17,18 @@ class ManageCoursePage extends React.Component {
     this.saveCourse = this.saveCourse.bind(this);
   }
 
+
+  // This is React lifecycle method
+  // Sometimes it may run even if props haven't changed
+  // but React thinks that they will
+  // that's why we need this 'if' statement
+  componentWillReceiveProps(nextProps) {
+    if (this.props.course.id != nextProps.course.id) {
+      // Necessary to populate form when existing course is loaded directly.
+      this.setState({course: Object.assign({}, nextProps.course)});
+    }
+  }
+
   updateCourseState(event) {
     const field = event.target.name;
     let course = this.state.course;
@@ -26,20 +38,20 @@ class ManageCoursePage extends React.Component {
 
   saveCourse(event) {
     event.preventDefault();
-
-    if (!this.courseFormIsValid()) {
-      return;
-    }
-
-    this.setState({saving: true});
-
-    this.props.actions.saveCourse(this.state.course)
-      .then(() => this.redirect())
-      .catch(error => {
-        toastr.error(error);
-        this.setState({saving: false});
-      });
+    this.props.actions.saveCourse(this.state.course);
+    this.context.router.push('/courses');
+    // .then(() => this.redirect())
+    //   .catch(error => {
+    //     toastr.error(error);
+    //     this.setState({saving: false});
+    //   });
   }
+
+  // redirect() {
+  //   this.setState({saving: false});
+  //   toastr.success('Course saved');
+  //   this.context.router.push('/courses');
+  // }
 
   render() {
     return (
@@ -47,6 +59,7 @@ class ManageCoursePage extends React.Component {
         allAuthors={this.props.authors}
         course={this.state.course}
         errors={this.state.errors}
+        onSave={this.saveCourse}
         onChange={this.updateCourseState}
       />
     );
@@ -55,12 +68,28 @@ class ManageCoursePage extends React.Component {
 
 ManageCoursePage.propTypes =  {
   course: PropTypes.object.isRequired,
-  authors: PropTypes.array.isRequired
+  authors: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
+//Pull in the React Router context so router is available on this.context.router.
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object
+};
+
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course) return course[0]; //since filter returns an array, have to grab the first.
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  let courseId = ownProps.params.id; // reading url params
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
 
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
   let authorsFormattedForDropdown = state.authors.map(author => {
     return {
       value: author.id, 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,9 +6,13 @@ import authors from './authorReducer';
 // we could use courses: courses
 // but because it's ES6 we can use this 
 // shorthand property name
+
 // !!!!!!!!!!!!!!! IMPORTANT !!!!!!!!!!!!!!!
 // Also this name is the name which
 // u use in Container component
+// In the other words these keys mean
+// what part of state each reducer is responsible for
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 const rootReducer = combineReducers({
   courses,
   authors
